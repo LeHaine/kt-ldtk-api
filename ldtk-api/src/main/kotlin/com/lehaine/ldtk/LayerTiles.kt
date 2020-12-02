00eb112fe87a30740fc80b4b224922e8d6ebc87d
LeHaine <colt.daily@gmail.com>
2020-12-02 17:43:49 -0500

    implement LayerTiles

--- ldtk-api/src/main/kotlin/com/lehaine/ldtk/LayerTiles.kt
+++ ldtk-api/src/main/kotlin/com/lehaine/ldtk/LayerTiles.kt
@@ -1,6 +1,32 @@
 package com.lehaine.ldtk
 
 class LayerTiles(json: LayerInstanceJson) : Layer(json) {
+    data class TileInfo(val tileId: Int, val flipBits: Int)
 
+    private val _tiles = mutableMapOf<Int, List<TileInfo>>()
+    val tiles get() = _tiles.toMap()
+
+    init {
+        json.gridTiles.forEach {
+            if (!_tiles.containsKey(it.d[0])) {
+                _tiles[it.d[0]] = mutableListOf(TileInfo(it.t, it.f))
+            } else {
+                val mutList = _tiles[it.d[0]] as MutableList
+                mutList.add(TileInfo(it.t, it.f))
+            }
+        }
+    }
+
+    fun getTileStackAt(cx: Int, cy: Int): List<TileInfo> {
+        return if (isCoordValid(cx, cy) && _tiles.contains(getCoordId(cx, cy))) {
+            _tiles[getCoordId(cx, cy)] ?: emptyList()
+        } else {
+            emptyList()
+        }
+    }
+
+    fun hasAnyTileAt(cx: Int, cy: Int): Boolean {
+        return _tiles.contains(getCoordId(cx, cy))
+    }
 
 }
