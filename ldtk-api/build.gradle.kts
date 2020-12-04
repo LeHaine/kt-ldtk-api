1fa37fa770b43dffbef21c241fa03554f9760906
LeHaine <colt.daily@gmail.com>
2020-12-04 17:37:16 -0500

    add shadowjar to ldtk-api and ldtk-processor

--- ldtk-api/build.gradle.kts
+++ ldtk-api/build.gradle.kts
@@ -1,8 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.20"
     kotlin("kapt") version "1.4.20"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 repositories {
@@ -16,7 +18,21 @@ dependencies {
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
 }
 
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    var kotlinPath = ""
+    dependencies {
+        kotlinPath = kotlin("stdlib").toString()
+    }
+    withType<ShadowJar> {
+        minimize()
+        dependencies {
+            exclude(dependency(kotlinPath))
+            exclude(project(":ldtk-api"))
+            exclude("**/*.kotlin_*")
+        }
+        archiveClassifier.set("")
+    }
 }
--- ldtk-processor/build.gradle.kts
+++ ldtk-processor/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.4.20"
     kotlin("kapt") version "1.4.20"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 group = "com.lehaine"
@@ -16,4 +20,23 @@ dependencies {
     implementation("com.squareup:kotlinpoet:1.7.2")
     implementation("com.google.auto.service:auto-service:1.0-rc7")
     kapt("com.google.auto.service:auto-service:1.0-rc7")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    var kotlinPath = ""
+    dependencies {
+        kotlinPath = kotlin("stdlib").toString()
+    }
+    withType<ShadowJar> {
+        minimize()
+        dependencies {
+            exclude(dependency(kotlinPath))
+            exclude(project(":ldtk-api"))
+            exclude("**/*.kotlin_*")
+        }
+        archiveClassifier.set("")
+    }
 }
