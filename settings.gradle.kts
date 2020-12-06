58327ba427eec006f55d69186630c9a3f5471f84
LeHaine <colt.daily@gmail.com>
2020-12-06 14:13:42 -0500

    update settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "gdx-ldtk-api"
 include("ldtk-api")
-if (System.getenv()["JITPACK"] != null)
+if (System.getenv()["JITPACK"] == null)
     include("samples")
 include("ldtk-processor")
 include("libgdx-backend")
